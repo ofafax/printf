2a44050e5ebd8072176001f010cca2ed627eef02
ofafax <98403358+ofafax@users.noreply.github.com>
2023-04-01 03:51:28 +0300

    upd

--- get_flags.c
+++ get_flags.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
+ * get_flags - Calculates all active flags
  * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * @i: takes a parameter.
+ * Return: Flags
  */
 int get_flags(const char *format, int *i)
 {
